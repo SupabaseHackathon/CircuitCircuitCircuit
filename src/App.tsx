99a3c17ee28c48c630557a93f07e8ca36dfaed00
MagnusHJensen <magnus.holm.jensen@lego.dk>
2022-12-11 12:52:35 +0100

    Update PR workflow to checkout repo

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR - Cool
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
